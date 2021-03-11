0a236ecb6cab1d90c8a7922e4e7de3ba2075d10e
Aliaksandr Mamchyts <al.mamchyts@gmail.com>
2021-03-11 20:20:02 +0300

    feat: implement function reverse

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
 module.exports = function reverse (n) {
-  
-  
+    const int = n;
+    const str = n.toString();
+    let arr = str.split('');
+    arr = arr.reverse();
+    if (arr[arr.length-1] == '-') {
+        arr.pop();
+    }
+    const result = arr.join('');
+    return result;
 }
